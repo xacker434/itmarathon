a1c5baf5bbee6ced0a9f5330226473c11a83917c
xacker434 <deniszaviazkin@gmail.com>
2025-11-11 20:10:44 +0200

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "terraform-tfstate-jkbskefbjek"
+    bucket       = "terraform-tfstate-itmarathon2025"
     key          = "terraform.tfstate"
     region       = "eu-central-1"
     use_lockfile = true
